2911d7cbddc6f4971906e3d6c78285340a278b63
lancefun <103starwars@gmail.com>
2018-09-07 20:20:21 -0400

    the tester

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("Socket server running...");
+
+const socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log("new connection: " + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+
+}
