a2ed5e775fba374659e138a3587792d3628573f3
lancefun <103starwars@gmail.com>
2018-11-22 12:52:09 -0500

    Added clear button

--- public/main.js
+++ public/main.js
@@ -1,17 +1,28 @@
 var socket;
+var button;
 
 function setup() {
     createCanvas(600, 400);
     background(51);
 
+    button = createButton("Clear");
+    button.mousePressed(function() {
+        socket.emit('clear');
+    });
+
     socket = io.connect();
     socket.on('mouse', newDrawing)
+    socket.on('clear', clearScreen);
 }
 
 function newDrawing(data) {
     noStroke();
     fill(255, 0, 100);
     ellipse(data.x, data.y, 10, 10);
+}
+
+function clearScreen() {
+    background(51);
 }
 
 function mouseDragged() {
@@ -25,7 +36,4 @@ function mouseDragged() {
     noStroke();
     fill(255);
     ellipse(mouseX, mouseY, 10, 10);
-}
-
-function draw() {
 }
--- server.js
+++ server.js
@@ -18,10 +18,15 @@ function newConnection(socket) {
     console.log("new connection: " + socket.id);
 
     socket.on('mouse', mouseMsg);
+    socket.on('clear', clearCanvas);
 
     function mouseMsg(data) {
         socket.broadcast.emit('mouse', data);
         console.log(data);
     }
 
+    function clearCanvas() {
+        io.emit('clear');
+    }
+    
 }
